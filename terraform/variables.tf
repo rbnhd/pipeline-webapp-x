428b2fc5c8ef6a72a8a8b78cc48b919abf06dd65
vikram.kushwaha <vikram.kushwaha>
2024-04-24 14:59:40 +0900

    validate

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,15 @@
 variable "project_id" {
   description = "The ID of the GCP project"
+  type        = string
 }
 
 variable "region" {
   description = "The GCP region"
   default     = "asia-northeast1"
+  type        = string
 }
 
 variable "credentials_file_path" {
   description = "The path to the service account key file"
+  type        = string
 }

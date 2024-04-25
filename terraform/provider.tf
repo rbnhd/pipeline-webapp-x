3f49fbe3a66d34859ad5792d4e36e153c059b9a2
Vikram Kushwaha <vikramrobvikki@gmail.com>
2024-04-25 17:20:19 +0900

    check

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -12,7 +12,6 @@ terraform {
 provider "google" {
   project = var.project_id
   region  = var.region
-  credentials = file(env("GOOGLE_APPLICATION_CREDENTIALS")) # use the environment variable
   # credentials = file(var.credentials_file_path)
 }
 

4862481013641027c17f6c142bcbc8b827a73693
Vikram Kushwaha <vikramrobvikki@gmail.com>
2024-04-25 13:36:22 +0900

    vpc and components

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,24 +1,28 @@
 # This network.tf will create a VPC network and a firewall rule that allows SSH traffic.
-resource "google_compute_network" "vpc_network" {
-  name                    = "vpc-network"
-  auto_create_subnetworks = "true"
+resource "google_compute_network" "vpc" {
+  name                    = "${var.project_id}-vpc"
+  auto_create_subnetworks = "false"
+
+  # The VPC should depend on the required API's, Just in case, if it's not enabled already. 
+  depends_on = [
+    google_project_service.compute_api,
+    google_project_service.container_api,
+  ]
 }
 
-resource "google_compute_subnetwork" "subnetwork" {
-  name          = "subnet"
-  ip_cidr_range = "10.2.0.0/16"
-  network       = google_compute_network.vpc_network.self_link
+resource "google_compute_subnetwork" "subnet" {
+  name          = "${var.project_id}-subnet"
   region        = var.region
+  network       = google_compute_network.vpc.name
+  ip_cidr_range = "10.10.0.0/24"
 }
 
 resource "google_compute_firewall" "default_allow_ssh" {
-  name    = "default-allow-ssh"
-  network = google_compute_network.vpc_network.self_link
-
+  name    = "${var.project_id}-default-allow-ssh"
+  network = google_compute_network.vpc.self_link
   allow {
     protocol = "tcp"
     ports    = ["22"]
   }
-
   source_ranges = ["0.0.0.0/0"]
 }

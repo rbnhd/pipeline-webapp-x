4c96bc1234e7768e734a710092f8ffd15a3b2980
Vikram Kushwaha <vikramrobvikki@gmail.com>
2024-04-25 22:29:26 +0900

    uncoment chnages

--- terraform/kubernetestf.tf
+++ terraform/kubernetestf.tf
@@ -1,69 +1,69 @@
-# # This file will contain the configuration for the Kubernetes cluster and node pool.
-# variable "gke_username" {
-#   default     = ""
-#   description = "gke username"
-# }
+# This file will contain the configuration for the Kubernetes cluster and node pool.
+variable "gke_username" {
+  default     = ""
+  description = "gke username"
+}
 
-# variable "gke_password" {
-#   default     = ""
-#   description = "gke password"
-# }
+variable "gke_password" {
+  default     = ""
+  description = "gke password"
+}
 
-# variable "gke_num_nodes" {
-#   default     = 1
-#   description = "number of gke nodes"
-# }
+variable "gke_num_nodes" {
+  default     = 1
+  description = "number of gke nodes"
+}
 
-# # GKE cluster
-# data "google_container_engine_versions" "gke_version" {
-#   location = var.region
-#   version_prefix = "1.27."
-# }
+# GKE cluster
+data "google_container_engine_versions" "gke_version" {
+  location = var.region
+  version_prefix = "1.27."
+}
 
-# resource "google_container_cluster" "primary" {
-#   name     = "${var.project_id}-gke"
-#   location = var.region
+resource "google_container_cluster" "primary" {
+  name     = "${var.project_id}-gke"
+  location = var.region
 
-#   # We can't create a cluster with no node pool defined, but we want to only use
-#   # separately managed node pools. So we create the smallest possible default node pool and immediately delete it.
-#   remove_default_node_pool = true
-#   initial_node_count       = 1
-#   node_config {
-#     disk_size_gb = 10
-#     # Use preemptible for lowest cost (if testing)
-#     preemptible  = true
-#     machine_type = "e2-micro"
-#     tags         = ["gke-node", "${var.project_id}-gke", "default-node-gke"]
-#   }
-#   network    = google_compute_network.vpc.name
-#   subnetwork = google_compute_subnetwork.subnet.name
-#   deletion_protection = false
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count       = 1
+  node_config {
+    disk_size_gb = 10
+    # Use preemptible for lowest cost (if testing)
+    preemptible  = true
+    machine_type = "e2-micro"
+    tags         = ["gke-node", "${var.project_id}-gke", "default-node-gke"]
+  }
+  network    = google_compute_network.vpc.name
+  subnetwork = google_compute_subnetwork.subnet.name
+  deletion_protection = false
 
-# }
+}
 
-# # Separately Managed Node Pool
-# resource "google_container_node_pool" "primary_nodes" {
-#   name       = google_container_cluster.primary.name
-#   location   = var.region
-#   cluster    = google_container_cluster.primary.name
+# Separately Managed Node Pool
+resource "google_container_node_pool" "primary_nodes" {
+  name       = google_container_cluster.primary.name
+  location   = var.region
+  cluster    = google_container_cluster.primary.name
 
-#   version = data.google_container_engine_versions.gke_version.release_channel_latest_version["STABLE"]
-#   node_count = var.gke_num_nodes
-#   node_config {
-#     oauth_scopes = [
-#       "https://www.googleapis.com/auth/logging.write",
-#       "https://www.googleapis.com/auth/monitoring",
-#     ]
-#     labels = {
-#       env = var.project_id
-#     }
-#     disk_size_gb = 10
-#     # Use preemptible for lowest cost (if testing)
-#     preemptible  = true
-#     machine_type = "e2-micro"
-#     tags         = ["gke-node", "${var.project_id}-gke"]
-#     metadata = {
-#       disable-legacy-endpoints = "true"
-#     }
-#   }
-# }
+  version = data.google_container_engine_versions.gke_version.release_channel_latest_version["STABLE"]
+  node_count = var.gke_num_nodes
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+    labels = {
+      env = var.project_id
+    }
+    disk_size_gb = 10
+    # Use preemptible for lowest cost (if testing)
+    preemptible  = true
+    machine_type = "e2-micro"
+    tags         = ["gke-node", "${var.project_id}-gke"]
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+  }
+}

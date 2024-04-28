afacb4684b082066bceb329de300fafec5ef81ad
Vikram Kushwaha <vikramrobvikki@gmail.com>
2024-04-28 23:12:55 +0900

    disable log config

--- terraform/network.tf
+++ terraform/network.tf
@@ -28,11 +28,6 @@ resource "google_compute_subnetwork" "subnet" {
     ip_cidr_range = "192.168.1.0/24"
   }
 
-  log_config {
-    aggregation_interval = "INTERVAL_10_MIN"
-    flow_sampling        = 0.5
-    metadata             = "INCLUDE_ALL_METADATA"
-  }
 }
 
 resource "google_compute_firewall" "allow_nodeports" {
